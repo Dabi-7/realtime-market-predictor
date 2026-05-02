5d8707162f3c799c42d7d244b6d9110337e40655
carbon <malikawaishahbaz@gmail.com>
2026-05-02 15:48:58 +0500

    updated ingestion pipeline script and alphvantage file and tracked new data

--- run_ingestion.sh
+++ run_ingestion.sh
@@ -17,11 +17,10 @@ echo "Tracking new data with dvc......"
 dvc add data/yahoo_finance_historical.csv
 dvc add data/rss_news_sentiment.csv
 dvc add data/av_timeseries.csv
-dvc add data/av_news_sentiment.csv
 
 echo "pushing updates to github"
 git add data/*.csv
-git commit -m "feat: added alphavantage to ingestion pipeline to bypass reddit api blocker"
+git commit -m "feat: finalized data ingestion for time-series and RSS sentiment"
 
 git push origin main
 
--- src/ingestion/fetch_alphavantage.py
+++ src/ingestion/fetch_alphavantage.py
@@ -8,10 +8,10 @@ load_dotenv()
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 def fetch_alpha_vantage_data(symbol="SPY"):
-    print(f"Fetching data for {symbol} via Alpha Vantage...")
+    print(f"Fetching time-series data for {symbol} via Alpha Vantage...")
     os.makedirs('data', exist_ok=True)
 
-    #Fetching Time-Series Market Data
+    #Fetch Time-Series Market Data
     ts_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
     ts_response = requests.get(ts_url).json()
 
@@ -20,9 +20,9 @@ def fetch_alpha_vantage_data(symbol="SPY"):
         df_ts = pd.DataFrame.from_dict(ts_data, orient='index')
         df_ts.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
         df_ts.index.name = 'Date'
-        df_ts = df_ts.apply(pd.to_numeric) # Convert strings to floats
+        df_ts = df_ts.apply(pd.to_numeric)
 
-        # targets
+        #targets
         df_ts['Price_Movement'] = df_ts['Close'] - df_ts['Open']
         df_ts['Direction'] = df_ts['Price_Movement'].apply(lambda x: 'Up' if x > 0 else 'Down')
         df_ts['Volatility'] = df_ts['High'] - df_ts['Low']
@@ -30,34 +30,8 @@ def fetch_alpha_vantage_data(symbol="SPY"):
         df_ts.to_csv('data/av_timeseries.csv')
         print("Saved Alpha Vantage Time-Series data.")
     else:
-        print("Error fetching time-series. (Check API limits)")
-
-    #Fetching News & Sentiment Data
-    news_url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={symbol}&apikey={API_KEY}"
-    news_response = requests.get(news_url).json()
-
-    if "feed" in news_response:
-        news_data = []
-        for item in news_response["feed"]:
-
-            score = float(item.get("overall_sentiment_score", 0))
-            
-
-            if score >= 0.15: sentiment = "Positive"
-            elif score <= -0.15: sentiment = "Negative"
-            else: sentiment = "Neutral"
-
-            news_data.append({
-                "Date": item["time_published"],
-                "Headline": item["title"],
-                "Sentiment": sentiment
-            })
-
-        df_news = pd.DataFrame(news_data)
-        df_news.to_csv('data/av_news_sentiment.csv', index=False)
-        print("Saved Alpha Vantage News & Sentiment data.")
-    else:
-        print("Error fetching news. (Check API limits)")
+        
+        print(f"Error fetching time-series: {ts_response}")
 
 if __name__ == "__main__":
     fetch_alpha_vantage_data()
